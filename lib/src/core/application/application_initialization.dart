69cc3c46f0bd443e578ef347d26f7b051ad0c4e9
Ruslan <Ruslan.Chizhov@raimbek.com>
2020-12-15 14:14:58 +0600

    changed readme

--- lib/src/core/application/application_initialization.dart
+++ lib/src/core/application/application_initialization.dart
@@ -29,7 +29,6 @@ class _ApplicationInitializationState extends State<ApplicationInitialization> {
 
   @override
   Widget build(BuildContext context) {
-    Element
     return BlocProvider<SunPointBLoC>(
         create: (BuildContext context) => SunPointBLoC(
             getDay: GetDay(DayRepositoryImplementation(
